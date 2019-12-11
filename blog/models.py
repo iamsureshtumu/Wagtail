5f4ca8aaec776730267b496387ddd1fa021dae66
suresh <sureshtumu3691@gmail.com>
2019-12-11 13:14:58 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,116 @@
+from django.db import models
+from django import forms
+
+# New imports added for ClusterTaggableManager, TaggedItemBase, MultiFieldPanel
+
+from modelcluster.fields import ParentalKey,ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+
+"""
+Blogs generally display content in reverse chronological order
+We want to make sure we’re only displaying published content.
+To accomplish these things, we need to do more than just grab the index page’s children in the template.
+Instead, we’ll want to modify the QuerySet in the model definition. 
+Wagtail makes this possible via the overridable get_context() method. Modify your BlogIndexPage model like this:
+"""
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+    intro = RichTextField(blank=True)
+
+
+    def get_context(self, request):
+         # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Blog information"),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+#Arya : The ParentalKey to BlogPage is what attaches the gallery images to a specific page
+#Arya : ImageChooserPanel, which provides a pop-up interface for choosing an existing image or uploading a new one.
+#Arya : Finally, adding the InlinePanel to BlogPage.content_panels makes the gallery images available on the editing interface for BlogPage
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        ImageChooserPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
---
+++ home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+#body is defined as RichTextField, a special Wagtail field
+#content_panels define the capabilities and the layout of the editing interface
